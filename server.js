23055ce260d467886fa264127eaebda3f106bfa3
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-07-11 01:31:42 +0530

    feat: Add body-parser dependency for request parsing
    This commit adds the "body-parser" dependency to the project, which is used for parsing incoming request bodies. This will enable the server to handle JSON and URL-encoded data sent in the request body.
    
    Co-authored-by: [Author Name]

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+import bodyParser from 'body-parser'
+//import .env
+import 'dotenv/config'
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({extended: true }));
+
+app.use(router)
+
+//connecting to database
+import DB_CONNECTION from './database/db_connection.js'
+DB_CONNECTION();
+
+app.listen(process.env.PORT,()=>{
+    console.log('Server is running on port 3000');
+})
