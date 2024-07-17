45b46324dad8f62deb26910491f6d0572a730da0
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-07-17 11:48:46 +0530

    Update verifyJWT middleware to use authorization header and user

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,12 +1,17 @@
 import jwt from 'jsonwebtoken'
- const verifyJWT = (req, res, next) => {
-    const token = req.header('auth-token');
+import User from '../models/user.model.js'
+ const verifyJWT = async (req, res, next) => {
+    
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    
     if(!token){
         return res.status(401).send('Access Denied');
     }
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.user = verified;
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+        const user = await User.findById(decodedToken.id)
+        req.user = user;
+        console.log("token verified..")
         next();
     } catch (error) {
         res.status(400).send('Invalid Token');
