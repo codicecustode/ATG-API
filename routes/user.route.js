5c6915c8a0098c1da4cef3b56e77d4e1f0055542
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-07-17 18:53:04 +0530

    Add routes and controllers for post, comment, and like functionality
    This commit adds routes and controllers for creating, updating, and deleting posts, comments, and likes. It also includes the necessary middleware for file uploads using Multer. The new functionality allows users to create posts, update post captions, add comments to posts, view all comments on a post, get a specific comment by its ID, update their own comments, delete their own comments, like posts, and view all likes on a post. These enhancements improve the interactivity and engagement of the application.

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,35 @@
 import Router from 'express'
 const router = Router();
+//import uploadFile from '../middleware/upload.middleware.js'
+import {upload} from '../middleware/multer.middleware.js'
+import verifyJWT from '../middleware/verifyJWT.js'
 import {register, login, forgotUserPassword} from '../controllers/user.controller.js'
+import {createPost, getAllPost, getPostById, updateCaptionOfPost, deletePost} from '../controllers/post.controller.js'
+import {likePost, getAllLike} from '../controllers/like.controller.js'
+import {createComment, getAllCommentsOnPost, getCommentById, updateComment, deleteComment} from '../controllers/comment.controller.js'
 router.route('/signup').post(register)
 router.route('/login').post(login)
 router.route('/reset-password').post(forgotUserPassword)
+
+//for post controller
+router.route('/post').post(verifyJWT, upload.single('image'), createPost)
+router.route('/update-post-caption/:id').put(verifyJWT, updateCaptionOfPost)
+router.route('/get-all-post').get(verifyJWT, getAllPost)
+router.route('/post/:id').get(verifyJWT, getPostById)
+router.route('/delete-post/:id').delete(verifyJWT, deletePost)
+
+//for comment controller
+router.route('/add-comment/:id').post(verifyJWT, createComment)
+router.route('/get-all-comment-on-post/:id').get(getAllCommentsOnPost)
+router.route('/get-comment/:id').get(getCommentById)
+router.route('/update-comment/:id').put(verifyJWT, updateComment)
+router.route('/delete-comment/:id').delete(verifyJWT, deleteComment)
+
+//for like controller
+router.route('/like-post/:id').post(verifyJWT, likePost)
+router.route('/post/get-all-like/:id').get(getAllLike)
+
+    
+    
 
 export default router
