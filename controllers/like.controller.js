3d07b15ec9aee72b4e1cdaaf4f8e4a368ade3cfe
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-07-17 18:52:13 +0530

    Add error handling for missing postId in likePost controller

--- controllers/like.controller.js
+++ controllers/like.controller.js
@@ -1,51 +1,55 @@
 import Like from '../models/like.model.js'
 
 export const likePost = async (req, res) => {
+    const postId = req.params.id;
+
+    if(!postId){
+        return res.status(404).send('Post not found');
+    }
+
     const like = new Like({
         user: req.user._id,
-        post: req.params.id
+        post: postId
     })
     try {
         const savedLike = await like.save();
-        res.status(201).send(savedLike);
+        res.status(201).json({
+            message: 'Post liked successfully',
+            savedLike
+        });
     } catch (error) {
         res.status(400).send(error);
     }
 }
 
-//get all the like
+//get all the like on a single post
 export const getAllLike = async (req, res) => {
     try {
-        const likes = await Like.find().populate('user', 'username');
+        const postId = req.params.id;
+
+        if(!postId){
+            return res.status(404).send('Post not found');
+        }
+        const likes = await Like.find({ post: postId }).populate('user', 'username');
         res.status(200).send(likes);
     } catch (error) {
         res.status(400).send(error);
     }
 }
 
-//get like by id
-export const getLikeById = async (req, res) => {
-    try {
-        const like = await Like.findById(req.params.id).populate('user', 'username');
-        res.status(200).send(like);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-}
-
-//delet like
-export const deleteLike = async (req, res) => {
-    try {
-        const like = await Like.findById(req.params.id);
-        if(like.user.toString() === req.user._id.toString()){
-            await like.deleteOne();
-            res.status(200).send('Like has been deleted');
-        } else {
-            res.status(401).send('You can delete only your like');
-        }
-    } catch (error) {
-        res.status(400).send(error);
-    }
-}
+//delete like
+// export const deleteLike = async (req, res) => {
+//     try {
+//         const like = await Like.findById(req.params.id);
+//         if(like.user.toString() === req.user._id.toString()){
+//             await like.deleteOne();
+//             res.status(200).send('Like has been deleted');
+//         } else {
+//             res.status(401).send('You can delete only your like');
+//         }
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// }
 
 
