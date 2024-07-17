c17bf3de6d964fd2bf564e008b9c4041d164245f
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-07-17 18:49:46 +0530

    Add comment functionality and update comment controller
    This commit adds the functionality to create, update, and delete comments on posts. It also includes the necessary changes to the comment controller to handle these operations. The code now allows users to create comments on posts, view all comments on a post, and get a specific comment by its ID. Additionally, users can update their own comments and delete them if they are the comment owner. This feature enhances the interactivity and engagement of the application.

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,43 +1,136 @@
 import Comment from '../models/comment.model.js'
+import Post from '../models/post.model.js'
 
 export const createComment = async (req, res) => {
+    const postId = req.params.id
     const comment = new Comment({
         user: req.user._id,
-        post: req.params.id,
+        post: postId,
         comment: req.body.comment
     })
+    console.log(comment.user)
     try {
-        const savedComment = await comment.save();
-        res.status(201).send(savedComment);
+        const newAddedComment = await comment.save(); 
+        const updatedPost = await Post.findByIdAndUpdate(
+            postId,
+            { $push: { comments: newAddedComment._id } }, // Add the comment ID to the 'comments' array
+            { new: true } // Return the updated post document
+        ).populate('comments'); // Populate the 'comments' field in the updated post
+        if (!updatedPost) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
+        res.status(201).json({
+            message: 'Comment added successfully',
+            comment: newAddedComment,
+            updatedPost
+        });
+        
     } catch (error) {
         res.status(400).send(error);
     }
 }
+
+//get all comments on a post
+export const getAllCommentsOnPost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        if(!postId){
+            return res.status(404).send('Post not found')
+        }
+
+        const comments = await Comment.find({ post: postId }).populate('user', 'username');
+        res.status(200).send(comments);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+//get comment by id
+export const getCommentById = async (req, res) => {
+    const commentId = req.params.id;
+
+    try {
+        const comment = await Comment.findById(commentId)
+
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+
+        const {user, post, comment: text, createdAt} = comment
+        const commentDetails = { user, post, text, createdAt}
+
+        // Send the comment data in the response
+        res.json({ commentDetails });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
 
 //update comment 
 export const updateComment = async (req, res) => {
     try {
         const comment = await Comment.findById(req.params.id);
-        if(comment.user.toString() === req.user._id.toString()){
-            await comment.updateOne({
-                $set: req.body
+
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+
+        // Check if the comment belongs to the user
+        if (comment.user.toString() === req.user._id.toString()) {
+            const updatedComment = await Comment.findByIdAndUpdate(
+                req.params.id,
+                { $set: { comment: req.body.editedComment } },
+                { new: true }  // Return the updated document
+            );
+
+            if (!updatedComment) {
+                return res.status(500).json({ message: 'Failed to update comment' });
+            }
+
+            return res.status(200).json({
+                message: 'Comment updated successfully',
+                updatedComment
             });
-            res.status(200).send('Comment has been updated');
         } else {
-            res.status(401).send('You can update only your comment');
+            return res.status(401).json({ message: 'You can update only your comment' });
         }
     } catch (error) {
-        res.status(400).send(error);
+        console.error('Error updating comment:', error);
+        return res.status(500).json({ message: 'Internal server error' });
     }
-}
+};
 
 //delete comment 
 export const deleteComment = async (req, res) => {
     try {
-        const comment = await Comment.findById(req.params.id);
+        const commentId = req.params.id;
+        const comment = await Comment.findById(commentId);
+
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+
+        const postId = comment.post;
+
         if(comment.user.toString() === req.user._id.toString()){
+            //remove comment from post
+            const post = await Post.findById(postId);
+
             await comment.deleteOne();
-            res.status(200).send('Comment has been deleted');
+
+            //delete the comment from post array in post schema
+            const updatedPost = await Post.findByIdAndUpdate(
+                postId,
+                { $pull: { comments: commentId } },
+                { new: true }
+            )
+            res.status(200).json({
+                message: 'Comment deleted successfully',
+                updatedPost
+            });
         } else {
             res.status(401).send('You can delete only your comment');
         }
